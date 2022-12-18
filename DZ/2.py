5f9bc5e1561017b7d49e42c97f5907c065e3fe88
YaroSLAVE <tys_brembo@mail.ru>
2022-12-18 17:36:01 +0400

    done task 2

--- DZ/2.py
+++ DZ/2.py
@@ -1,3 +1,8 @@
 # Напишите программу для проверки ложности утверждения
 # (W ⋀ Z) ⋁ ¬Y ⋁ (¬X ≡ ¬W) для всех значений предикат.
 
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            if not (x or y or z) == (not x and not y and not z):
+                print(f'истинно при x={x}, y={y}, z={z}')
