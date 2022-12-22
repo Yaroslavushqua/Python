9211bd9874294ffb134b829e89830aeb8e4c40c9
YaroSLAVE <tys_brembo@mail.ru>
2022-12-22 22:00:23 +0400

    done task DZ 2

--- Cem_2/DZ/2.py
+++ Cem_2/DZ/2.py
@@ -1,3 +1,13 @@
 # Напишите программу, которая принимает на вход число N
 # и выдает набор произведений чисел от 1 до N в виде списка.
 
+num = int(input("Введите число: "))
+
+sum = 1
+numList = []
+
+for i in range(num):
+    sum *= i + 1
+    numList.append(sum)
+
+print(numList)
