51fadd04006b4070ea384e30e24b115f6ee5ec0d
YaroSLAVE <tys_brembo@mail.ru>
2022-12-25 11:28:16 +0400

    done task DZ 1

--- Cem_3/DZ/1.py
+++ Cem_3/DZ/1.py
@@ -2,3 +2,15 @@
 # пользователь.Напишите программу, которая найдёт сумму элементов списка, 
 # стоящих на нечётных позициях(не индексах).
 
+sizeNum = int(input('Введите размер списка: '))
+numList = []
+sum = 0
+for i in range(sizeNum):
+     list_number = int(input(f'Введите число {i+1} '))
+     numList.append(list_number)
+     if i % 2 != 0:
+         sum += numList[i]
+
+
+print(numList)
+print(f'Сумма чисел на нечетных позициях = {sum}')
