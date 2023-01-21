eb97ea28469afa81d473861ad53fd1ee7745d306
YaroSLAVE <llapbkob@vk.com>
2023-01-21 13:09:59 +0400

    done DZ task 1

---
+++ Cem_6/DZ/1.py
@@ -0,0 +1,10 @@
+# 1. Представлен список чисел. Необходимо вывести элементы исходного списка, 
+# значения которых больше предыдущего элемента. Use comprehension.
+
+from random import sample
+len_list = int(input("Ведите кол-во чисел в рандомном списке: "))
+my_list = [i for i in sample(range(1, len_list*2), len_list)]
+print("Рандомный список: ", my_list)
+sort_list = [my_list[i]
+             for i in range(1, len(my_list)) if my_list[i] > my_list[i-1]]
+print(sort_list)
