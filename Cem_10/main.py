fa70393193bb64f4add12b374240fed9f6ed11a4
YaroSLAVE <llapbkob@vk.com>
2023-02-12 16:40:59 +0400

    done main

--- Cem_10/main.py
+++ Cem_10/main.py
@@ -29,3 +29,32 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     global value
     global keyboard
     query = update.callback_query
+
+    await query.answer()
+
+    data = query.data
+    row = int(data[0])
+    col = int(data[1])
+
+    win = model.do_turn(model.player_mode, col, row)
+    if win == '':
+        win = model.bot_turn()
+        
+    await context.bot.edit_message_text(chat_id=query.message.chat.id, message_id=query.message.id, text=model.get_status(), reply_markup=model.get_table())
+
+
+TOKEN = None
+with open("token.txt") as f:
+    TOKEN = f.read().strip()
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+
+app.add_handler(CommandHandler("help", help))
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("playerX", playerX))
+app.add_handler(CommandHandler("player0", player0))
+
+app.add_handler(CallbackQueryHandler(button))
+
+app.run_polling()
